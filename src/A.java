8b8774c270cd5e9ee45fbab942d829c5b8a7a707
arkawa <arturkawa111@gmail.com>
2023-01-07 11:05:42 +0100

    add

---
+++ src/A.java
@@ -0,0 +1,21 @@
+public class A {
+
+    void show() {
+        System.out.println("A");
+    }
+
+}
+
+class B extends A {
+
+    void show() {
+        System.out.println("B");
+    }
+
+    public static void main(String[] args) {
+        B b = new B();
+        b.show();
+
+    }
+
+}
